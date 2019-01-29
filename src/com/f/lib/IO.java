f76704dab518b2a5ee80787085231a7533e3cdaa
fsancheztemprano <fsancheztemprano@danielcastelao.org>
2019-01-29 08:59:33 +0100

    IO

--- src/com/f/lib/IO.java
+++ src/com/f/lib/IO.java
@@ -72,11 +72,23 @@ public abstract class IO {
             return false;
         }
     }
-
+    /**
+     * Comprueba si el parametro str es parseable a int, si lo es, devuelve 
+     * el int, de lo contrario devuelve null
+     * 
+     * @param str string a parsear a int
+     * @return str parseado a int
+     */
     public static int stringToInt(String str) {
         return isInteger(str) ? Integer.parseInt(str) : null;
     }
-
+    /**
+     * Comprueba si el argumento str es float
+     * 
+     * @param str String a ser comprobado
+     * @return boolean true si es float
+     * @exception NumberFormatException e si str no es float
+     */
     public static boolean isFloat(String str) {
         try {
             Float.parseFloat(str);
